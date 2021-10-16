8afada0e9bc8a27858c0cff8ea591806c0686cdc
JSONRete <jsonrete@gmail.com>
2021-10-16 07:19:26 -0400

    commit tree b6b77c1a8edbb9ce606d99ee9cc631ee6661fc13

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,15 +1,23 @@
+// ./src/reducers/counterReducer
+
 export default function counterReducer(
 	state = {
-		clicks: 0
+	  clicks: 0
 	},
 	action
-) {
+  ) {
+	console.log(1, action);
 	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				clicks: state.clicks + 1
-			}
-		default:
-			return state;
+	  case "INCREASE_COUNT":
+		console.log(2, "Current state.clicks %s", state.clicks);
+		console.log(3, "Updating state.clicks to %s", state.clicks + 1);
+		return {
+		  clicks: state.clicks + 1
+		};
+  
+	  default:
+		console.log(4, "Initial state.clicks: %s", state.clicks);
+		return state;
 	}
-}
+  }
+  
